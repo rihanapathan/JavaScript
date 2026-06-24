ae7f881cf3b8066113aca3910c7c743b8c775749
rihanapathan <rihanapathan3510@gmail.com>
2026-06-24 11:34:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+//Task 1 - Employee Name
+
+// let EmployeeName = prompt("What is your name")
+
+// console.log("EmployeeName : ",EmployeeName)
+
+// document.writeln("EmployeeName : Welcome ",EmployeeName)
+
+// Task 2 - Company Entry Confirmation
+
+//let CompanyEntryConfirmation = confirm("Are you ready to join today's Javascript training?")
+
+// console.log(CompanyEntryConfirmation )
+
+//Task 3 - Customer Greeting
+
+// alert("Welcome to Stackly Solutions!")
+
+// console.log("Customer entered the website.")
+
+//Task 4 - Student Details
+
+// let StudentName = prompt("Name")
+
+// let StudentAge = prompt("Age")
+
+// console.log("Name : ", StudentName)
+
+// console.log("Age :", StudentAge)
+
+// Task 5 - Product Price
+
+// let ProductPrice = 1500
+
+// UpdatedPrice = 2000
+
+// console.log(UpdatedPrice)
+
+//Task 6 - Login Validation
+
+// let username = prompt("Enter the UserName")
+
+// if (username === "" || username === null) {
+//     console.warn("Warning: Username is empty.");
+// } 
+
+// else {
+//     console.log("Login Successful: " , username);
+// }
+
+//Task 7 - Website Maintenance
+
+// alert("Website is under maintenance.")
+
+// console.error("Error : Website is currently unavailable.")
+
+//Task 8 - Feedback Collection
+
+// let Feedback = prompt("How was taday's Javascript session?")
+
+// console.log("Feedback : ",Feedback)
+
+// document.writeln("Thank you for your feedback!")
+
+//Task 9 - Profile Information
+
+// let Name = prompt("Name")
+// let City = prompt("City")
+// let FavoriteProgrammingLanguage = prompt("FavoriteProgrammingLanguage")
+
+// console.log("Name : ",Name)
+// console.log("City : ",City)
+// console.log("FavoriteProgrammingLanguage  : ",FavoriteProgrammingLanguage)
+
+//Task 10 - Mini Registration Form
+
+console.log("=====Registration Details=====")
+
+let Name = prompt("Name")
+let Email = prompt("Email")
+let Mobile = prompt("mobile")
+
+console.log("Name : ",Name)
+console.log("Email : ",Email)
+console.log("Mobile :",Mobile)
+
+alert("Registration Successful")
